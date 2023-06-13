4e17f505ac8a60eb6f600b54098ce8eecb56a6b5
Joaquin Gavernet <joaquingavernet@gmail.com>
2023-06-13 16:24:00 -0300

    Correccion de codigo (indentacion).

--- modulo2/practica4/ejercicio5.s
+++ modulo2/practica4/ejercicio5.s
@@ -7,10 +7,10 @@ res: .word 0
 dadd r1, r0, r0
 ld r2, cant(r0)
 loop: ld r3, datos(r1)
-daddi r2, r2, -1
-dsll r3, r3, 1
-sd r3, res(r1)
-daddi r1, r1, 8
-bnez r2, loop
+  daddi r2, r2, -1
+  dsll r3, r3, 1
+  sd r3, res(r1)
+  daddi r1, r1, 8
+  bnez r2, loop
 daddi r4, r4, 1
 halt
